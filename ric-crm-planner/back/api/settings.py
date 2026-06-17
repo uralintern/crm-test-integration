10fe8122d6c66504f82cc8c99f64b53e2bc1d56d
amave423 <artem123antropov@mail.ru>
2026-06-17 14:17:55 +0500

    increased lifetime of token

--- ric-crm-planner/back/api/settings.py
+++ ric-crm-planner/back/api/settings.py
@@ -230,7 +230,7 @@ SWAGGER_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=6),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
